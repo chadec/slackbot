9e096c207ff832efb14f690a91bc1a83c87310b6
chadec <chadec@users.noreply.github.com>
2016-08-15 12:48:40 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@
 // Initialize DTR Modules
 var settings = {};
 
-settings.slackToken = 'slack-token-here';
+settings.slackToken = 'xoxb-39881074755-Iha4X5yCtMGcTz5go4jo0Bu9';
 settings.apiPath = 'http://api.destinytrialsreport.com/';
 settings.trigger = '!';
 //slack handles
